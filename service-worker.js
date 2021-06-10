c3053d030d08fc9cf17d3347871aba3000c3a932
CSIProjectDemo <85685237+CSIProjectDemo@users.noreply.github.com>
2021-06-10 09:50:36 -0400

    j
    j

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AnthonyGithubCorner/CSIPatientDashboard/precache-manifest.779ab7f26ef8c9728efd8dc85cce3ed4.js"
+  "/CSIProjectDemo/CSIWebsiteDemo/precache-manifest.779ab7f26ef8c9728efd8dc85cce3ed4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AnthonyGithubCorner/CSIPatientDashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CSIProjectDemo/CSIWebsiteDemo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
